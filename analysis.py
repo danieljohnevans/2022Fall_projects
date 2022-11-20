d671cd90590d9d65642c8081125076b8ca2ac679
danieljohnevans <djevan11@gmail.com>
2022-11-20 17:43:19 -0500

    initial commit

---
+++ analysis.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+
+
+# skeleton code before meeting will most likely change significantly
+
+def read_booknlp(booknlp: str) -> pd.DataFrame:
+    """function to read in bookNLP data and return a dataframe of gender and related words
+    :param booknlp
+    :return
+    """
+
+    pass
+
+
+def read_books(books: str):
+    """function to read in book metadata
+    :param books
+    """
+    pass
+
+
+def count_words(gender: pd.DataFrame):
+    """function to count most frequent words identified per gender
+        :param gender
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
