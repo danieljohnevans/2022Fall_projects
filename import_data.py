b479afc175f62da6628ff4097bd8cc0816d1e7b0
danieljohnevans <djevan11@gmail.com>
2022-12-04 22:15:58 -0600

    updating import_data

--- import_data.py
+++ import_data.py
@@ -1,8 +1,10 @@
 import os
+import pathlib
 import pickle
 
 import pandas as pd
 import pandas as pickle
+import json
 
 """
 This file imports our bookNLP data and outputs it as a pickle. If available, it also connects metadata to our 
@@ -14,8 +16,30 @@ def read_booknlp(booknlp: str) -> pd.DataFrame:
     :param booknlp
     :return
     """
+    ps = pathlib.Path(booknlp)
 
-    pass
+    allfiles = list(ps.glob('*/*/*.book'))
+
+    files = [p for p in allfiles if p.is_file()]
+
+    # print(allfiles)
+
+    for f in allfiles:
+        t = open(f.as_posix())
+        temp_json = json.load(t)
+        # print(temp_json)
+        lhd_book = pd.concat([pd.DataFrame(temp_json),
+                              pd.json_normalize(temp_json['characters'])],
+                             axis=1)
+
+        # drop characters column
+        lhd_book = lhd_book.drop('characters', axis=1)
+        # lhd_book.set_index('id' , inplace=True)
+
+        #do we want to return as single dataframe or multiple?
+        # return ldh_book
+        print(lhd_book)
+
 
 def read_metadata(metadata:str) -> pd.DataFrame:
     """
@@ -34,4 +58,5 @@ def mutate_data(booknlp:pd.DataFrame, metadata:pd.DataFrame) -> pickle:
     """
 
 if __name__ == '__main__':
+    read_booknlp('data/')
     pass
