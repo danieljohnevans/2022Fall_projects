6af70e8f5a813b58178ba2d95916d00cd6bfb8db
cbelitz <clarabelitz1@gmail.com>
2022-12-06 17:17:34 -0600

    trying to make graphs :(

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 
@@ -20,10 +22,22 @@ def read_books(books: str):
     pass
 
 
+def combine_book_and_entity(booknlp: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
 def count_words(gender: pd.DataFrame):
     """function to count most frequent words identified per gender
         :param gender
 
+    """
+    pass
+
+
+def make_gender_graphs(chars: pd.DataFrame):
+    """
+    function to take a processed character dataframe and make a bar graph of gender inferences
+    :param chars:
     """
     pass
 
